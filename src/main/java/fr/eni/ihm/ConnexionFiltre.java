edcd33e263044b996195dc925598418cb3c3ae56
Thomas  BOURVIC <thomas.bourvic2023@campus-eni.fr>
2023-09-19 11:52:57 +0200

    filtre

---
+++ src/main/java/fr/eni/ihm/ConnexionFiltre.java
@@ -0,0 +1,63 @@
+package fr.eni.ihm;
+import java.io.IOException;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+@WebFilter(
+        urlPatterns = {
+                "/profile",
+                "/encheres/*",
+                "/suppressionProfile",
+                "/ventes",
+               
+        },
+        dispatcherTypes = {
+        		DispatcherType.REQUEST
+        }
+)
+public class ConnexionFiltre  implements Filter {
+	
+	public void init(FilterConfig config) throws ServletException {
+    }
+	
+	public void destroy() {
+    }
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+       
+        if (isUserLoggedIn(httpRequest)) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/connexion.jsp"); // Redirige vers la page d'accueil
+        }
+    }
+
+	public static boolean isUserLoggedIn(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        
+        if (session != null && session.getAttribute("utilisateurConnecte") != null) {
+   
+            return true;
+        } else {
+           
+            return false;
+        }
+    }
+}
